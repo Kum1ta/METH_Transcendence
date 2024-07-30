a0e52b07dae126e046116ec6a7c2502d2ef3f468
Kum1ta <eddydhj@gmail.com>
2024-07-31 01:09:19 +0200

    Module livechat :     - Style finish     - Just button "new conversation" not ready     - Starting websocket but its

--- site/module_livechat/site/main.js
+++ site/module_livechat/site/main.js
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.js                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edbernar <edbernar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 13:50:35 by edbernar          #+#    #+#             */
-/*   Updated: 2024/07/30 19:55:40 by edbernar         ###   ########.fr       */
+/*   Updated: 2024/07/31 00:37:11 by edbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,9 +60,7 @@ function	liveChat() {
 		chatDiv.style.display = "none";
 	});
 
-	// showListUserMessage(userList);
-	console.warn("Retirer showActualGameMessage() et remettre showListUserMessage()")
-	showActualGameMessage();
+	showListUserMessage(userList);
 	privateButtonChatHome.addEventListener("click", () => {
 		gameButtonChatHome.removeAttribute("id");
 		privateButtonChatHome.setAttribute("id", "selected");
@@ -77,8 +75,12 @@ function	liveChat() {
 
 function	showListUserMessage(userList) {
 	const	divMessageListChatHome = document.getElementById("messageListChatHome");
+	let		divUser;
 
+	divMessageListChatHome.style.height = "100%";
+	divMessageListChatHome.style.paddingBottom = "10px";
 	divMessageListChatHome.innerHTML = '';
+	divMessageListChatHome.scrollTop = 0;
 	userList.forEach(element => {
 		divMessageListChatHome.innerHTML += `
 			<div class="user">
@@ -87,8 +89,15 @@ function	showListUserMessage(userList) {
 				</div>
 				<h3>${element.name}</h3>
 			</div>
-		`;
+		`;		
 	});
+	divMessageListChatHome.innerHTML += "<p style='text-align: center; margin-top: 20px;'>New conversation +</p>";
+	divUser = document.getElementsByClassName("user");
+	for (let i = 0; i < divUser.length; i++) {
+		divUser[i].addEventListener("click", () => {
+			launchPrivateChat(userList[i]);
+		});
+	}
 }
 
 function	showActualGameMessage() {
@@ -111,29 +120,32 @@ function	showActualGameMessage() {
 				content: "Hey",
 				date: "19:21 30/07/2024"
 			},
-			// {
-			// 	from: "Astropower",
-			// 	content: "Do you want play ?",
-			// 	date: "19:22 30/07/2024"
-			// },
-			// {
-			// 	from: "Kumita",
-			// 	content: "Yes, i'm ready !",
-			// 	date: "19:22 30/07/2024"
-			// },
-			// {
-			// 	from: "Kumita",
-			// 	content: "The game was too hard but well played",
-			// 	date: "19:27 30/07/2024"
-			// },
-			// {
-			// 	from: "Astropower",
-			// 	content: "Yeah but you still won. See you soon",
-			// 	date: "19:27 30/07/2024"
-			// },
+			{
+				from: "Astropower",
+				content: "Do you want play ?",
+				date: "19:22 30/07/2024"
+			},
+			{
+				from: "Kumita",
+				content: "Yes, i'm ready !",
+				date: "19:22 30/07/2024"
+			},
+			{
+				from: "Kumita",
+				content: "The game was too hard but well played",
+				date: "19:27 30/07/2024"
+			},
+			{
+				from: "Astropower",
+				content: "Yeah but you still won. See you soon",
+				date: "19:27 30/07/2024"
+			},
 		]
 	}; //Remplace temporairement la requete qui devra être de la meme forme
 
+
+	divMessageListChatHome.style.height = "230px";
+	divMessageListChatHome.style.paddingBottom = "20px";
 	divMessageListChatHome.innerHTML = '';
 	if (request.isInGame === false)
 	{
@@ -142,14 +154,104 @@ function	showActualGameMessage() {
 	}
 	request.listMessage.forEach(element => {
 		divMessageListChatHome.innerHTML += `
-			<div class="${element.from === me ? "meMessage" : "opponentMessage"}">
-				<p class="content">${element.content}</p>
-				<p class="time">${element.date}</p>
-			</div>
+		<div class="${element.from === me ? "meMessage" : "opponentMessage"}">
+		<p class="content">${element.content}</p>
+		<p class="time">${element.date}</p>
+		</div>
 		`;
 	});
+	divMessageListChatHome.scrollTop = divMessageListChatHome.scrollHeight;
 	divMessageListChatHome.innerHTML += `
-		<div id="inputDiv" placeholder="Enter your message here"></div>
+		<div id="inputMessageDiv">
+			<textarea type="text" id="inputMessage" placeholder="Enter your message here"></textarea>
+			<p id="sendButton">\></p>
+		</div>
 	`;
-	divMessageListChatHome.setAttribute("contenteditable", "true");
+}
+
+function	launchPrivateChat(user) {
+	const	divMessageListChatHome = document.getElementById("messageListChatHome");
+	const	divButtonTypeChatHome = document.getElementById("buttonTypeChatHome");
+	let		returnButton;
+	let		me = "Kumita";
+	let		request = {
+		opponent: {
+			name: user.name,
+			pfp: user.pfp
+		},
+		listMessage: [
+			{
+				from: user.name,
+				content: "Salut !",
+				date: "10:05 31/07/2024"
+			},
+			{
+				from: "Kumita",
+				content: "Hey",
+				date: "10:05 31/07/2024"
+			},
+			{
+				from: user.name,
+				content: "Tu veux coder un peu ?",
+				date: "10:06 31/07/2024"
+			},
+			{
+				from: "Kumita",
+				content: "Ouais, je suis partant !",
+				date: "10:06 31/07/2024"
+			},
+			{
+				from: "Kumita",
+				content: "Ce bug était vraiment galère à résoudre, mais on y est arrivé.",
+				date: "10:45 31/07/2024"
+			},
+			{
+				from: user.name,
+				content: "Ouais, mais t'as trouvé la solution. À la prochaine !",
+				date: "10:46 31/07/2024"
+			},
+		]
+	}; //Remplace temporairement la requete qui devra être de la meme forme
+
+	let h2Button = divButtonTypeChatHome.getElementsByTagName("h2");
+	let len = h2Button.length;
+	for (let i = 0; i < len; i++) {
+		h2Button[i - i].remove();
+	}
+	divButtonTypeChatHome.innerHTML += `
+		<h2>${user.name}</h2>
+		<p id="returnButton" style="margin: 8px 10px 0 0; text-align: right;">Return</p>
+	`;
+	h2Button[0].style.cursor = "default";
+	returnButton = document.getElementById("returnButton");
+	returnButton.style.cursor = "pointer";
+	returnButton.addEventListener("click", () => {
+		divButtonTypeChatHome.innerHTML = `
+			<h2 id="selected">Private</h2>
+			<h2>Game</h2>
+		`;
+		liveChat();
+	});
+
+
+
+	divMessageListChatHome.style.height = "230px";
+	divMessageListChatHome.style.paddingBottom = "20px";
+	divMessageListChatHome.innerHTML = '';
+	request.listMessage.forEach(element => {
+		divMessageListChatHome.innerHTML += `
+		<div class="${element.from === me ? "meMessage" : "opponentMessage"}">
+		<p class="content">${element.content}</p>
+		<p class="time">${element.date}</p>
+		</div>
+		`;
+	});
+	divMessageListChatHome.scrollTop = divMessageListChatHome.scrollHeight;
+	divMessageListChatHome.innerHTML += `
+		<div id="inputMessageDiv">
+			<textarea type="text" id="inputMessage" placeholder="Enter your message here"></textarea>
+			<p id="sendButton">\></p>
+		</div>
+	`;
+
 }
