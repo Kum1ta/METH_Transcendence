7bee4f6519b519b261045b2d6805e7eb8d814765
Kum1ta <eddydhj@gmail.com>
2024-08-03 23:51:09 +0200

    Websocket :     - Connect with token : good     - Connect with mail/pass : good but wait for front     - Open private msg : good but it cant send message     - fixed some bug on printDebug on class User on wsServer

--- site/module_livechat/server/typeRequets/getPrivateListUser.py
+++ site/module_livechat/server/typeRequets/getPrivateListUser.py
@@ -6,7 +6,7 @@
 #    By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/08/03 15:10:23 by edbernar          #+#    #+#              #
-#    Updated: 2024/08/03 17:07:21 by edbernar         ###   ########.fr        #
+#    Updated: 2024/08/03 22:30:13 by edbernar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,37 +18,43 @@ data = [
 		{
 			"name": "Nessundorma",
 			"status": "online",
-			"pfp": "https://wallpapers-clan.com/wp-content/uploads/2023/05/cool-pfp-02.jpg"
+			"pfp": "https://wallpapers-clan.com/wp-content/uploads/2023/05/cool-pfp-02.jpg",
+			"id": 145564
 		},
 		{
 			"name": "Succotash",
 			"status": "offline",
-			"pfp": "https://i.pinimg.com/200x/28/75/96/287596f98304bf1adc2c411619ae8fef.jpg"
+			"pfp": "https://i.pinimg.com/200x/28/75/96/287596f98304bf1adc2c411619ae8fef.jpg",
+			"id": 256981
 		},
 		{
 			"name": "Astropower",
 			"status": "online",
-			"pfp": "https://ashisheditz.com/wp-content/uploads/2024/03/cool-anime-pfp-demon-slayer-HD.jpg"
+			"pfp": "https://ashisheditz.com/wp-content/uploads/2024/03/cool-anime-pfp-demon-slayer-HD.jpg",
+			"id": 301547
 		},
 		{
 			"name": "Assaultive",
 			"status": "offline",
-			"pfp": "https://i1.sndcdn.com/artworks-1Li0JIJrQGlojD3y-AEiNkw-t500x500.jpg"
+			"pfp": "https://i1.sndcdn.com/artworks-1Li0JIJrQGlojD3y-AEiNkw-t500x500.jpg",
+			"id": 432448
 		},
 		{
 			"name": "Redshock",
 			"status": "offline",
-			"pfp": "https://cdn.pfps.gg/pfps/7094-boy-pfp.png"
+			"pfp": "https://cdn.pfps.gg/pfps/7094-boy-pfp.png",
+			"id": 543211
 		},
 		{
 			"name": "Parley",
 			"status": "offline",
-			"pfp": "https://pbs.twimg.com/media/EscE6ckU0AA-Uhe.png"
+			"pfp": "https://pbs.twimg.com/media/EscE6ckU0AA-Uhe.png",
+			"id": 654123
 		},
 ]
 
-async def getPrivateListUser(userClass):
+async def getPrivateListUser(userClass, content=None):
 	# |TOM| Faire une requête à la base de données pour récupérer la liste des
 	# utilisateurs qui doivent apparaitre dans la liste du chat privé
 	# (ceux qui ont eu conversation avec l'utilisateur)
-	await userClass.send({"type": "get_private_list_user", "content": data})
+	await userClass.send({"type": "private_list_user", "content": data})
--- site/module_livechat/site/typeResponse/typeLogin.js
+++ site/module_livechat/site/typeResponse/typeLogin.js
@@ -6,13 +6,20 @@
 /*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 00:39:53 by edbernar          #+#    #+#             */
-/*   Updated: 2024/08/02 00:46:21 by edbernar         ###   ########.fr       */
+/*   Updated: 2024/08/03 23:37:33 by edbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+let userMeInfo = {
+	username: "",
+	id: 42
+};
 
 function	typeLogin(content)
 {
 	console.log("Welcome " + content.username + "\nYou're id is " + content.id);
+	userMeInfo.username = content.username;
+	userMeInfo.id = content.id;
 }
 
-export { typeLogin };
+export { userMeInfo, typeLogin };
