c1ae52d503e2ef4638a85fa47a7d398c5df4cded
Kum1ta <eddydhj@gmail.com>
2024-08-12 20:48:28 +0200

    Site     - 3d login in comment now

--- site/interface/site/login/createThreeDiv.js
+++ site/interface/site/login/createThreeDiv.js
@@ -3,56 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   createThreeDiv.js                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 18:09:36 by edbernar          #+#    #+#             */
-/*   Updated: 2024/08/10 18:30:31 by marvin           ###   ########.fr       */
+/*   Updated: 2024/08/12 20:46:53 by edbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-import * as THREE from 'three';
+// import * as THREE from 'three';
 
 function	createThreeDiv()
 {
 	const	divThree	= document.createElement("div");
 
 	divThree.setAttribute("id", "threeDiv");
-	const scene = new THREE.Scene();
-	// const camera = new THREE.PerspectiveCamera(75, divThree.innerWidth / divThree.innerHeight, 0.1, 1000);
-	const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-	const renderer = new THREE.WebGLRenderer( {antialias: true} );
-	// renderer.setSize(divThree.innerWidth, divThree.innerHeight);
-	renderer.setSize(200, 700);
-	divThree.appendChild(renderer.domElement);
-	renderer.setClearColor(0x020202);
+	// const scene = new THREE.Scene();
+	// // const camera = new THREE.PerspectiveCamera(75, divThree.innerWidth / divThree.innerHeight, 0.1, 1000);
+	// const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+	// const renderer = new THREE.WebGLRenderer( {antialias: true} );
+	// // renderer.setSize(divThree.innerWidth, divThree.innerHeight);
+	// renderer.setSize(200, 700);
+	// divThree.appendChild(renderer.domElement);
+	// renderer.setClearColor(0x020202);
 
-	let geometrie = new THREE.BoxGeometry(1, 1, 1);
+	// let geometrie = new THREE.BoxGeometry(1, 1, 1);
 	
-	let materiel = new THREE.MeshBasicMaterial({color:0xffffff});
+	// let materiel = new THREE.MeshBasicMaterial({color:0xffffff});
 	
-	const mesh = new THREE.Mesh( geometrie, materiel)
+	// const mesh = new THREE.Mesh( geometrie, materiel)
 
-	camera.position.set(0, 0 ,4);
+	// camera.position.set(0, 0 ,4);
 
-	renderer.antialias
+	// renderer.antialias
 
-	loop()
+	// loop()
 
-	function loop(){
-		requestAnimationFrame(loop);
-		let xsize = divThree.offsetWidth;
-		let ysize = divThree.offsetHeight;
-		renderer.setSize(xsize, ysize);
-		camera.aspect = xsize / ysize;
-		camera.updateProjectionMatrix()
-		console.log(xsize, ysize);
-		mesh.rotation.y += 0.001;
-		mesh.rotation.x += 0.0005;
-		renderer.render(scene, camera);
-	}
+	// function loop(){
+	// 	requestAnimationFrame(loop);
+	// 	let xsize = divThree.offsetWidth;
+	// 	let ysize = divThree.offsetHeight;
+	// 	renderer.setSize(xsize, ysize);
+	// 	camera.aspect = xsize / ysize;
+	// 	camera.updateProjectionMatrix()
+	// 	console.log(xsize, ysize);
+	// 	mesh.rotation.y += 0.001;
+	// 	mesh.rotation.x += 0.0005;
+	// 	renderer.render(scene, camera);
+	// }
 
-	scene.add(mesh);
-	renderer.render(scene, camera);
+	// scene.add(mesh);
+	// renderer.render(scene, camera);
 	return (divThree);
 }
 
--- site/interface/site/login/main.js
+++ site/interface/site/login/main.js
@@ -6,7 +6,7 @@
 /*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 17:40:15 by edbernar          #+#    #+#             */
-/*   Updated: 2024/08/10 17:57:45 by edbernar         ###   ########.fr       */
+/*   Updated: 2024/08/10 18:40:40 by edbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ function	login()
 	let		nodeText		= null;
 
 	waitForLogin().then((token) => {
-		console.log("login.js: userMeInfo.id = " + userMeInfo.id);
 		nodeText = document.createTextNode(userMeInfo.username);
 
 		if (token !== undefined)
