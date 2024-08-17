fe58722e91d8acc4f6182c2800fd2d60350b8881
tomoron <tomoron@student.42angouleme.fr>
2024-08-17 17:41:24 +0200

    there is a homepage now

---
+++ docker-compose/requirements/djangoserver/file/server/server/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class TaMereLaPute(models.Model):
+	test=models.CharField(max_length=200)
--- docker-compose/requirements/djangoserver/file/server/server/settings.py
+++ docker-compose/requirements/djangoserver/file/server/server/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vgcqdf^%(+@t*+cof@755e#q9p)myir%z2s*e*ea*v^i(4pta9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
