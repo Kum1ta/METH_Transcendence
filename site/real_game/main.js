8da97c8e601922f8bc920d23f15380bd8d03d959
Kum1ta <eddydhj@gmail.com>
2024-08-18 16:19:27 +0200

    Game     - Added 2 mods on cameras

--- site/real_game/main.js
+++ site/real_game/main.js
@@ -6,14 +6,44 @@
 /*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 00:53:53 by edbernar          #+#    #+#             */
-/*   Updated: 2024/08/18 01:09:38 by edbernar         ###   ########.fr       */
+/*   Updated: 2024/08/18 15:52:03 by edbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-import { Player } from './Player'
+import * as THREE from 'three';
+import { Player } from './class/Player'
 
-const player = new Player(null);
+function createBarPlayer(color)
+{
+	const geometry	= new THREE.BoxGeometry(1, 0.1, 0.1);
+	const material	= new THREE.MeshPhysicalMaterial({color: color});
+	const mesh		= new THREE.Mesh(geometry, material);
 
-setInterval(() => {
-    player.update();
-}, 100);
+	return (mesh);
+}
+
+function loop()
+{
+	// controls.update();
+	player.update();
+	renderer.render(scene, player.camera);
+}
+
+
+const scene			= new THREE.Scene();
+const bar			= createBarPlayer(0xed56ea);
+const renderer		= new THREE.WebGLRenderer();
+const player		= new Player(bar, renderer);
+const spotLight		= new THREE.SpotLight(0xffffff, 1, 0, Math.PI / 4);
+const helper		= new THREE.SpotLightHelper(spotLight);
+// const controls		= new OrbitControls(player.camera, renderer.domElement);
+
+scene.add(player.object);
+spotLight.target = player.object;
+scene.add(spotLight);
+scene.add(helper);
+scene.background = new THREE.Color(0x1a1a1a);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+player.setCameraPosition(0, 0.5, 1)
+renderer.setAnimationLoop(loop)
