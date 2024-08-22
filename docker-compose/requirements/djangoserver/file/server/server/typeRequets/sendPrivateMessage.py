8ead51062715594c28747aa5be7a228218f9731c
tomoron <tomoron@student.42angouleme.fr>
2024-08-22 20:37:48 +0200

    start convertion to channels websocket and start using django sessions

---
+++ docker-compose/requirements/djangoserver/file/server/server/typeRequets/sendPrivateMessage.py
@@ -0,0 +1,35 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    sendPrivateMessage.py                              :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/08/04 13:44:11 by edbernar          #+#    #+#              #
+#    Updated: 2024/08/22 19:13:31 by tomoron          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+from datetime import datetime
+
+async def sendPrivateMessage(socket, content):
+	# |Tom| Requete pour vérifier si l'user existe 
+	# Si user existe pas, faire ça : await socket.sendError("User not found", 9008)
+	# Sinon l'ajouter à la base de données
+	# |Eddy| Si user existe, envoyer le message privé aux deux personnes concernées
+	# sachant que le receveur doit être connecté. Dans le cas contraire, uniquement
+	# l'envoyeur recevra le message.
+
+	try:
+		time = content["time"]
+		time = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S.%fZ")
+		time = time.strftime("%H:%M %d/%m/%Y")
+		jsonVar = {"type": "new_private_message", "content": {
+			"from": content["from"],
+			"channel": content["to"],
+			"content": content["content"],
+			"date": time
+		}}
+		await socket.send(jsonVar)
+	except Exception as e:
+		await socket.sendError("Invalid message sent", 9009, e)
--- docker-compose/requirements/djangoserver/file/server/server/views.py
+++ docker-compose/requirements/djangoserver/file/server/server/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 from .models import User 
 
 def index(request):
+	if(request.session.get("visited", False)):
+		print("already visited")
+	request.session["visited"] = True
+
 	return HttpResponse("AAAAAAAAAAAAAAAAAAAAAAA")
