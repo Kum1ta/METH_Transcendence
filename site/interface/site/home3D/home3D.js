2167298eb6b9670ef5ca19e1d6e4719ce98e3a2b
Kum1ta <eddydhj@gmail.com>
2024-08-23 02:24:41 +0200

    Game     - Progressing on tv on home page

--- site/interface/site/home3D/home3D.js
+++ site/interface/site/home3D/home3D.js
@@ -6,64 +6,80 @@
 /*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 17:19:17 by edbernar          #+#    #+#             */
-/*   Updated: 2024/08/22 19:33:44 by edbernar         ###   ########.fr       */
+/*   Updated: 2024/08/23 02:23:55 by edbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import * as THREE from 'three'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
-
-
-
-const loader = new GLTFLoader();
+import { Screen } from './Screen.js'
 
 const	scene			= new THREE.Scene();
 const	renderer		= new THREE.WebGLRenderer({antialias: true});
-const	camera			= new THREE.PerspectiveCamera(60, window.innerWidth / innerWidth);
-const	ambiantLight	= new THREE.AmbientLight(0xffffff, 0.1);
-const	spotLight		= new THREE.SpotLight(0xffffff, 100, 0, 0.5);
-const	helper			= new THREE.SpotLightHelper(spotLight);
-
+const	camera			= new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight);
+const	ambiantLight	= new THREE.AmbientLight(0xffffff, 0.16);
+const	screen			= new Screen(scene);
 
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(window.innerWidth, window.innerHeight);
-scene.add(helper);
 document.body.getElementsByClassName('homeSection')[0].appendChild(renderer.domElement);
-scene.background = new THREE.Color(0x325352)
-camera.position.set(-10, 10, -10);
-spotLight.position.set(-10, 10, -10);
-spotLight.castShadow = true;
-scene.add(spotLight);
-helper.update();
+scene.background = new THREE.Color(0x020202)
+camera.position.set(0, 2.5, -5);
+console.log(camera.rotation);
+camera.rotation.set(Math.PI + 0.2, 0, Math.PI);
 scene.add(ambiantLight);
-const controls = new OrbitControls(camera, renderer.domElement);
+
+
+document.addEventListener('resize', () => {
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+});
+
+/***** FOR DEBUGGING PURPOSES *****/
+
+document.addEventListener('keydown', (e) => {
+	if (e.key === 'w')
+		camera.position.z += 0.1;
+	if (e.key === 's')
+		camera.position.z -= 0.1;
+	if (e.key === 'a')
+		camera.position.x -= 0.1;
+	if (e.key === 'd')
+		camera.position.x += 0.1;
+	if (e.key === 'q')
+		camera.position.y += 0.1;
+	if (e.key === 'e')
+		camera.position.y -= 0.1;
+	if (e.key === 'ArrowUp')
+		camera.rotation.x += 0.1;
+	if (e.key === 'ArrowDown')
+		camera.rotation.x -= 0.1;
+	if (e.key === 'ArrowLeft')
+		camera.rotation.y += 0.1;
+	if (e.key === 'ArrowRight')
+		camera.rotation.y -= 0.1;
+	if (e.key === 'p')
+	{
+		console.log(camera.position);
+		console.log(camera.rotation);
+	}
+
+});
+
+/**********************************/
+
 
 function home3D()
 {
 	createPlane();
-	createBox();
-	loader.load( './modeles/tv.glb', (gltf) => {
-		const tv = gltf.scene.children[0];
-		console.log(tv);
-		tv.position.set(0, 0.68, 0);
-		tv.material = new THREE.MeshPhysicalMaterial({color: 0x222222});
-		tv.scale.set(0.1, 0.08, 0.12);
-		tv.castShadow = true;
-		tv.receiveShadow = true;
-		scene.add(tv);
-	}, undefined, function ( error ) {
-		console.error( error );
-	} );
-	
 	renderer.setAnimationLoop(loop)
 }
 
 function createPlane()
 {
-	const	geometry	= new THREE.PlaneGeometry(20, 20);
-	const	material	= new THREE.MeshPhysicalMaterial({side: THREE.DoubleSide, color: 0xffffff});
+	const	geometry	= new THREE.PlaneGeometry(500, 500);
+	const	material	= new THREE.MeshPhysicalMaterial({side: THREE.DoubleSide, color: 0x020202});
 	const	mesh		= new THREE.Mesh(geometry, material);
 
 	mesh.position.set(0, 0, 0);
@@ -72,22 +88,11 @@ function createPlane()
 	scene.add(mesh);
 }
 
-function createBox()
-{
-	const	geometry	= new THREE.BoxGeometry(1, 1, 1);
-	const	material	= new THREE.MeshPhysicalMaterial({color: 0xffffff});
-	const	mesh		= new THREE.Mesh(geometry, material);
-
-	mesh.position.set(-3, 0.5, -3);
-	mesh.receiveShadow = true;
-	scene.add(mesh);
-}
-
 
 function loop()
 {
-	controls.update();
 	renderer.render(scene, camera);
 }
+
 
 export { home3D };
