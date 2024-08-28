dc0b8fb95086e8e6b9e19d7128462238e3aac30b
Kum1ta <eddydhj@gmail.com>
2024-08-29 00:45:13 +0200

    Gane     - Continuing solo game

--- site/real_game/class/soloClass/Players.js
+++ site/real_game/class/soloClass/Players.js
@@ -6,7 +6,7 @@
 /*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 15:12:25 by edbernar          #+#    #+#             */
-/*   Updated: 2024/08/28 16:29:28 by edbernar         ###   ########.fr       */
+/*   Updated: 2024/08/29 00:25:50 by edbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,17 +40,18 @@ class Players
 
 	static update()
 	{
-		let		i	= 0;
+		const	limits	= 4.55; 
+		let		i		= 0;
 
 		while (i < pressedButton.length)
 		{
-			if (pressedButton[i] == 'w' && player1.position.z > -5.05)
+			if (pressedButton[i] == 'w' && player1.position.z > -limits)
 				player1.position.z -= speed;
-			else if (pressedButton[i] == 's' && player1.position.z < 5.05)
+			else if (pressedButton[i] == 's' && player1.position.z < limits)
 				player1.position.z += speed;
-			else if (pressedButton[i] == 'ArrowUp' && player2.position.z > -5.05)
+			else if (pressedButton[i] == 'ArrowUp' && player2.position.z > -limits)
 				player2.position.z -= speed;
-			else if (pressedButton[i] == 'ArrowDown' && player2.position.z < 5.05)
+			else if (pressedButton[i] == 'ArrowDown' && player2.position.z < limits)
 				player2.position.z += speed;
 			i++;
 		}		
@@ -59,14 +60,16 @@ class Players
 
 function newBarPlayer(nbPlayer)
 {
-	const	geometry	=	new THREE.BoxGeometry(0.1, 0.2, 2);
+	const	geometry	=	new THREE.BoxGeometry(0.3, 0.4, 2.5);
 	const	material	=	new THREE.MeshPhysicalMaterial({color: 0xffffff});
 	const	mesh		=	new THREE.Mesh(geometry, material);
 
+	mesh.castShadow = true;
+	mesh.receiveShadow = true;
 	if (nbPlayer == 1)
-		mesh.position.set(-12, 0.2, 0);
+		mesh.position.set(-12, 0.4, 0);
 	else
-		mesh.position.set(12, 0.2, 0);
+		mesh.position.set(12, 0.4, 0);
 	return (mesh);
 }
 
@@ -92,4 +95,4 @@ function remKeyInArr(e)
 		pressedButton.splice(i, 1);
 }
 
-export { Players };
+export { Players, player1, player2 };
