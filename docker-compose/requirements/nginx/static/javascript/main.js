5927c36e54060af63cc6124a8838f6e39748f1ee
Kum1ta <eddydhj@gmail.com>
2024-09-12 18:23:40 +0200

    Site     - Add button for logout     - somes change for websocket     - back to login page when account created successfully

--- docker-compose/requirements/nginx/static/javascript/main.js
+++ docker-compose/requirements/nginx/static/javascript/main.js
@@ -6,17 +6,18 @@
 /*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 13:50:35 by edbernar          #+#    #+#             */
-/*   Updated: 2024/08/25 19:09:02 by edbernar         ###   ########.fr       */
+/*   Updated: 2024/09/12 17:59:23 by edbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-import * as Socket from '/static/javascript/websocket.js';
+import { launchSocket } from '/static/javascript/websocket.js';
 import { Page } from '/static/javascript/Page.js';
 
 let		pageRenderer = null;
 
 document.addEventListener('DOMContentLoaded', () => {
 	pageRenderer = new Page();
+	launchSocket();
 });
 
 export { pageRenderer };
--- docker-compose/requirements/nginx/static/javascript/typeResponse/typeCreateAccount.js
+++ docker-compose/requirements/nginx/static/javascript/typeResponse/typeCreateAccount.js
@@ -6,15 +6,17 @@
 /*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 14:25:42 by edbernar          #+#    #+#             */
-/*   Updated: 2024/09/10 14:52:33 by edbernar         ###   ########.fr       */
+/*   Updated: 2024/09/12 17:04:35 by edbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import { createNotification as CN } from "/static/javascript/notification/main.js";
+import { changeWindowLoginBack } from "/static/javascript/login/main.js"
 
 function typeCreateAccount()
 {
 	CN.new("Account created", "Please check your emails (including spam) to verify your account.");
+	changeWindowLoginBack(null);
 }
 
 export { typeCreateAccount }
