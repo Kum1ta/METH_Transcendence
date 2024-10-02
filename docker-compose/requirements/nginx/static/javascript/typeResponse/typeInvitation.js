d3fcde1f0b0dcfa1f1b80d6a42bb4bb9720a7dea
Mathis Degryck <madegryc@2A1.42angouleme.fr>
2024-10-02 22:10:07 +0200

    Site     - Add real text to game mode     - Add WASD ZQSD select (front)

--- docker-compose/requirements/nginx/static/javascript/typeResponse/typeInvitation.js
+++ docker-compose/requirements/nginx/static/javascript/typeResponse/typeInvitation.js
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   typeInvitation.js                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: madegryc <madegryc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 00:22:31 by edbernar          #+#    #+#             */
-/*   Updated: 2024/09/28 16:50:17 by edbernar         ###   ########.fr       */
+/*   Updated: 2024/10/02 16:35:15 by madegryc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ import { pageRenderer } from '/static/javascript/main.js'
 
 function typeInvitation(content)
 {
-	CN.new("New invitation", content.username + " invit you to play !", null, () => {
+	CN.new("New invitation", content.username + " invited you to play !", null, () => {
 		pageRenderer.changePage('waitingGamePage', false, {username: content.username, id: content.invitor});
 	}, 'Join', 30000);
 }
