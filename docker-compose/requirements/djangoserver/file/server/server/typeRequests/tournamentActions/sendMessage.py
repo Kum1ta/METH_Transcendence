3f2f7c7d5f3e1609cd72adf2ffda52336ff282e4
tomoron <tomoron@student.42angouleme.fr>
2024-10-05 03:28:48 +0200

    add send message tournament request

--- docker-compose/requirements/djangoserver/file/server/server/typeRequests/tournamentActions/sendMessage.py
+++ docker-compose/requirements/djangoserver/file/server/server/typeRequests/tournamentActions/sendMessage.py
@@ -6,9 +6,12 @@
 #    By: tomoron <tomoron@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/10/04 18:05:27 by tomoron           #+#    #+#              #
-#    Updated: 2024/10/04 18:05:56 by tomoron          ###   ########.fr        #
+#    Updated: 2024/10/05 03:27:45 by tomoron          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 async def sendMessage(socket, content):
-	print("nope")
+	if("message" not in content):
+		socket.sendError("missing message field",9038) 
+		return
+	socket.tournament.sendMessage(socket, content["message"])
